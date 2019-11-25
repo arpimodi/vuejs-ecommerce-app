22019bd6c030c207b798ee952e86d83565baae25
Arpita Modi <arpitamodi96@gmail.com>
2019-11-25 18:23:37 +0530

    changing in js/app.js

--- js/app.js
+++ js/app.js
@@ -74,4 +74,15 @@ new Vue({
         
             return null;
         },
+        increaseQuantity: function(cartItem) {
+            cartItem.product.inStock--;
+            cartItem.quantity++;
+        },
+        decreaseQuantity: function(cartItem) {
+            cartItem.quantity--;
+            cartItem.product.inStock++;
         
+            if (cartItem.quantity == 0) {
+                this.removeItemFromCart(cartItem);
+            }
+        },
